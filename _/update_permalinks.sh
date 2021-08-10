20adf2f5de99abf4d3e00aca0c396bf83b039250
Coda-Coda <Coda-Coda@users.noreply.github.com>
2021-08-10 05:51:18 +0000

    deploy: 55763577b03ad633b00ecd1e37335c998568b525

---
+++ _/update_permalinks.sh
@@ -0,0 +1,16 @@
+for existingF in $(find ./* -name '*.md'); do
+  rm $existingF
+done
+
+IFS=$'\n'
+for f in $(find ../* -name '*.md'); do 
+	f2=${f%.md} # Remove the .md extension
+	f3=${f2:2} # Remove the two dots from something like ../index
+
+	fOut=`basename $f` # Gets the basename, for outputting in the _permalink directory.
+echo -n '---
+page:
+  headHtml: |
+    <meta http-equiv="refresh" content="0; url='$f3'">
+---' > "permalink_$fOut"
+done
