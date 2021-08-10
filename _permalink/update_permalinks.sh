9b7c62fb3c16501730be09d5957965f17d21d642
Daniel Britten <daniel.britten@outlook.com>
2021-08-10 16:46:08 +1200

    Fix for issues when handling files in subdirectories

--- _permalink/update_permalinks.sh
+++ _permalink/update_permalinks.sh
@@ -6,7 +6,7 @@ for f in $(find ../* -name '*.md'); do
 	f2=${f%.md} # Remove the .md extension
 	f3=${f2:2} # Remove the two dots from something like ../index
 
-	fOut=${f:1} # Remove the one extra . from something like ../index.md
+	fOut=`basename $f` # Gets the basename, for outputting in the _permalink directory.
 echo -n '---
 page:
   headHtml: |
