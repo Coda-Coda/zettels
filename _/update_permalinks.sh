854d495851873d19bc681957b45ecf4ca2dc7021
Coda-Coda <Coda-Coda@users.noreply.github.com>
2021-08-11 01:24:20 +0000

    deploy: ca47df319989057e2a60ed824aa5909032618f9a

--- _/update_permalinks.sh
+++ _/update_permalinks.sh
@@ -8,12 +8,15 @@ for f in $(find ../* -name '*.md'); do
 	f3=${f2:2} # Remove the two dots from something like ../index
 
 	fOut=`basename $f` # Gets the basename, for outputting in the _permalink directory.
-echo -n '---
-page:
-  headHtml: |
-    <script>
+cp $f "permalink_$fOut"
+
+sed -i 's/\[\[\([[:alnum:]_-][[:alnum:] _-]*\)\]\]/\[\[permalink_\1\]\]/g' "permalink_$fOut"
+
+echo '
+
+```{=html}
+<script>
     window.location.replace("/zettels'$f3'");
-    </script>
----
-## Redirecting...' > "permalink_$fOut"
+</script>
+```' >> "permalink_$fOut"
 done
